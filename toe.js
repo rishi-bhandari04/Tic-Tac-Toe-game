4a52ec280ee2267c4a25632334efe489d61518be
rishi-bhandari04 <160389544+rishi-bhandari04@users.noreply.github.com>
2024-03-13 11:27:32 +0530

    Add files via upload

---
+++ toe.js
@@ -0,0 +1,84 @@
+let boxes=document.querySelectorAll(".box");
+let resetbtn=document.querySelector("#reset-btn");
+let newGameBtn=document.querySelector("#new-btn");
+let msgContainer=document.querySelector(".msg-container");
+let msg=document.querySelector("#msg");
+
+let turnO=true;    //playerX,playerO
+
+const winpatterns=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+
+
+const resetgame = ()  =>{
+    turnO=true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",() =>{
+        if(turnO){
+            box.innerText="O";
+            turnO=false;
+        }
+        else{
+            box.innerText="X";
+            turnO=true;
+        }
+        box.disabled=true; //we have written this when we click on any button second time then the opposite of the text will be printed
+        checkWinner();
+
+    });
+});
+
+
+const disableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+};
+
+
+const enableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled= false;
+        box.innerText="";
+    }
+};
+
+const showWinner=(winner)=>{
+    msg.innerText=`congratulation, winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+
+
+const checkWinner= () =>{
+    for(let pattern of winpatterns){  
+        let pos1val=boxes[pattern[0]].innerText;
+        let pos2val=boxes[pattern[1]].innerText;
+        let pos3val=boxes[pattern[2]].innerText;
+
+        if(pos1val!="" && pos2val!="" && pos3val!=""){
+            if(pos1val===pos2val && pos2val===pos3val){
+                console.log(pos1val," is the Winner");
+                showWinner(pos1val);
+            }
+        }
+    }
+
+};
+
+
+newGameBtn.addEventListener("click",resetgame);
+resetbtn.addEventListener("click",resetgame);
